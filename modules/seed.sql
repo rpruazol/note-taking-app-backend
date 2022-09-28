6bb263521a47a55651c7146b6b68d36771f9fb85
rpruazol <rpruazol@gmail.com>
2022-09-27 23:14:39 -0400

    create board functionality and tweaked schema

--- modules/seed.sql
+++ modules/seed.sql
@@ -6,6 +6,8 @@ id serial PRIMARY KEY,
 title VARCHAR (255),
 description VARCHAR (255),
 user_id VARCHAR (255),
+board VARCHAR (255),
+board_id VARCHAR(255),
 created_at TIMESTAMP,
 updated_at TIMESTAMP
 );
@@ -22,14 +24,32 @@ created_at TIMESTAMP
 INSERT INTO notes (
   title,
   description,
+  board,
+  board_id,
   created_at
 )
 VALUES (
   'test note',
   'this is a test note to test saving to the database.  hello world.',
+  'To Do',
+  1,
   NOW()
 );
 
+INSERT INTO notes (
+  title,
+  description,
+  board,
+  board_id,
+  created_at
+)
+VALUES (
+  'other note',
+  'this is a note that should go in the "Other" board',
+  'Other',
+  2,
+  NOW()
+);
 
 
 INSERT INTO boards (
@@ -38,6 +58,16 @@ INSERT INTO boards (
 )
 VALUES (
   'To Do',
+  NOW()
+)
+RETURNING *;
+
+INSERT INTO boards (
+  name,
+  created_at
+)
+VALUES (
+  'Other',
   NOW()
 )
 RETURNING *;
